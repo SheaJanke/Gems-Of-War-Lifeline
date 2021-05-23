1e39862d0731166632837156e39ea57986ea657b
SheaJanke <sheajanke@gmail.com>
2021-05-22 22:10:13 -0700

    Opencv import.

---
+++ app/src/main/java/com/cowbraingames/optimalmatcher_gemsofwar/BoardDetection.java
@@ -0,0 +1,14 @@
+package com.cowbraingames.optimalmatcher_gemsofwar;
+
+import android.graphics.Bitmap;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+public class BoardDetection {
+
+    public BoardDetection(){
+        Mat mat = new Mat();
+        //Bitmap bmp32 = bmp.copy(Bitmap.Config.ARGB_8888, true);
+        //Utils.bitmapToMat(bmp32, mat);
+    }
+}
